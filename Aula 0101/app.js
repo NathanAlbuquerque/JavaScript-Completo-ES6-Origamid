b5494b0b8345fcc21c12ac1d8d8bb64eb17f7e4e
Nathan Albuquerque <nathan.nra@outlook.com>
2024-10-19 19:34:18 -0300

    Aula 0101: Apresentação do curso e da plataforma

---
+++ Aula 0101/app.js
@@ -0,0 +1,36 @@
+Esta aula basicamente é uma explicação acerca do curso e oque irei aprender aqui.
+
+Objetos:
+Constructor functions, prototype, String, Array, Math, Number, Function, Object.
+
+Efeitos no DOM:
+Modules, event bubble, setTimeout, forms e mais.
+
+JavaScript Assíncrono:
+Promisses, Fetch (novo AJAX), JSON, API, HTTP, Async / Await e mais.
+
+Classes:
+Class, contructor, get, set, extends e mais.
+
+Mias de JavaScript
+IIFE, factory function, clojures, debbuging, destruction, rest, interables.
+
+Regular Expression:
+Principais padrões e métoodos.
+
+Automação Front End:
+CLI, NPM, ESLint, Webpack, Babel e Git.
+
+Projeto Final:
+Refatoração do projeto utilizando classes. Praticando com Git, ESLint e mais.
+
+Slide:
+Criação do plugin de slide.
+
+O curso vai direto ao ponto?
+Não. O objeto do curso não é ir direto ao ponto, e sim fazer você entender o ponto. Eu rodo o ponto, volto no ponto e demonstro o ponto.
+
+
+Link da aula: https://www.origamid.com/curso/javascript-completo-es6/0101-javascript-completo-es6/
+Link da apresentação: https://www.origamid.com/slide/javascript-completo-es6/#/0101-javascript-completo-es6/1
+
