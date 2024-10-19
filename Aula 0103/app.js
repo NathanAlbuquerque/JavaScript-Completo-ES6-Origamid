b8a415edaab003c275b18a7425c9f3dbb73d2e44
Nathan Albuquerque <nathan.nra@outlook.com>
2024-10-19 20:06:40 -0300

    Aula 0103: Cofiguração do ambiente

---
+++ Aula 0103/app.js
@@ -0,0 +1,29 @@
+Nesta aula foi configurado o ambiente de codificação para o curso, não necessáriamente obrigatório seguir a risca, porém é indicado para acompanhar da forma mais aproxiada possível as aulas e o ambiente do professor.
+
+Instalação:
+ - VSCode
+ - Git
+ - Node
+
+Configuração do VSCode:
+ - Extenção Origamid Theme
+ - Extenção ive Server
+ - Extenção Color Highlights
+ - Extensão Prettier
+
+File > Preferenccias > Configurações
+
+{
+  "editor.fontFamily": "'IBM Plex Mono', Monaco, 'Courier New', monospace",
+  "editor.fontSize": 14,
+  "editor.lineHeight": 25,
+  "editor.tabSize": 2,
+  "editor.wordWrap": "on",
+  "workbench.colorTheme": "Origamid",
+  "workbench.startupEditor": "newUntitledFile",
+  "window.zoomLevel": 0,
+  "editor.colorDecorators": false,
+  "workbench.activityBar.visible": true,
+  "workbench.sideBar.location": "left",
+  "liveServer.settings.donotShowInfoMsg": true,
+}
